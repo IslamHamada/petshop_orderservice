a1dde4bd3e7ceb2b1935ea087178512ebfdf0a68
IslamHamada <Islamhamada222@gmail.com>
2025-11-11 17:40:29 +0100

    add Order repository

--- src/main/java/com/islamhamada/petshop/repository/OrderRepository.java
+++ src/main/java/com/islamhamada/petshop/repository/OrderRepository.java
@@ -1,4 +1,9 @@
 package com.islamhamada.petshop.repository;
 
-public class OrderRepository {
+import com.islamhamada.petshop.entity.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Long> {
 }
