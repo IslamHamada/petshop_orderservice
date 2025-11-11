8d2ed2e6a393bae52818f02078d4503d2b14ae4f
IslamHamada <Islamhamada222@gmail.com>
2025-11-11 17:40:15 +0100

    add order entity

--- src/main/java/com/islamhamada/petshop/entity/Order.java
+++ src/main/java/com/islamhamada/petshop/entity/Order.java
@@ -1,7 +1,22 @@
 package com.islamhamada.petshop.entity;
 
+import java.util.Date;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
 
 @Entity
+@Data
 public class Order {
+
+    @Id
+    @GeneratedValue
+    private long id;
+
+    private long userId;
+
+    private long cartItemId;
+
+    private Date date;
 }
