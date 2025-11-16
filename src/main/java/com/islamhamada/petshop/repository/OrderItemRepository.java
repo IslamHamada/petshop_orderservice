beae12cb3892191d85ccd27c23760bcca9634abc
IslamHamada <Islamhamada222@gmail.com>
2025-11-16 11:49:00 +0100

    OrderItemRepository + findAllByOrderId

---
+++ src/main/java/com/islamhamada/petshop/repository/OrderItemRepository.java
@@ -0,0 +1,12 @@
+package com.islamhamada.petshop.repository;
+
+import com.islamhamada.petshop.entity.OrderItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
+    List<OrderItem> findAllByOrderId(long id);
+}
