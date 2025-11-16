9cac49f3603cdbc8dd193504313a7a30961ca16d
IslamHamada <Islamhamada222@gmail.com>
2025-11-16 11:41:59 +0100

    add OrderItem entity

---
+++ src/main/java/com/islamhamada/petshop/entity/OrderItem.java
@@ -0,0 +1,26 @@
+package com.islamhamada.petshop.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderItem {
+    @Id
+    @GeneratedValue
+    private long id;
+
+    private long orderId;
+
+    private long productId;
+
+    private long count;
+}
