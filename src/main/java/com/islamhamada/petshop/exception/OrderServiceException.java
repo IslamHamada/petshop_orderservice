90e6a4963797eeb3fff72e74550b4a91bcc0a813
IslamHamada <Islamhamada222@gmail.com>
2025-12-02 23:03:53 +0100

    correct OrderServiceException to be similar to the exception classes of other services

--- src/main/java/com/islamhamada/petshop/exception/OrderServiceException.java
+++ src/main/java/com/islamhamada/petshop/exception/OrderServiceException.java
@@ -1,16 +1,16 @@
 package com.islamhamada.petshop.exception;
 
+import com.islamhamada.petshop.contracts.exception.ServiceException;
 import lombok.Data;
 import org.springframework.http.HttpStatus;
 
 @Data
-public class OrderServiceException extends RuntimeException {
+public class OrderServiceException extends ServiceException {
     private long errorCode;
     private HttpStatus httpStatus;
 
     public OrderServiceException(String message, long errorCode, HttpStatus httpStatus) {
-        super(message);
-        this.errorCode = errorCode;
+        super(message, "ORDER_" + errorCode);
         this.httpStatus = httpStatus;
     }
 }
