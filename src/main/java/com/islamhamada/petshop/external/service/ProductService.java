0db30ba0c2eee3e8acc8ef0ae49b2b7f90e5105a
IslamHamada <islamhamada222@gmail.com>
2026-01-10 10:39:10 +0100

    add reduceProductQuantity put mapping

--- src/main/java/com/islamhamada/petshop/external/service/ProductService.java
+++ src/main/java/com/islamhamada/petshop/external/service/ProductService.java
@@ -1,10 +1,13 @@
 package com.islamhamada.petshop.external.service;
 
+import com.islamhamada.petshop.contracts.model.ReduceQuantityRequest;
 import com.islamhamada.petshop.contracts.dto.ProductDTO;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @FeignClient(
         name = "product-service-svc",
@@ -14,4 +17,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 public interface ProductService {
     @GetMapping("/{id}")
     ResponseEntity<ProductDTO> getProductById(@PathVariable("id") long id);
+
+    @PutMapping("/{id}")
+    ResponseEntity<ProductDTO> reduceProductQuntity(@PathVariable("id") long id, @RequestBody ReduceQuantityRequest request);
 }
