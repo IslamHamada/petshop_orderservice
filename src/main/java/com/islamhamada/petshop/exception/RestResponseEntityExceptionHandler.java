9b2d82f0eb1f979755331b3e3861e5e6b7299a6a
IslamHamada <islamhamada222@gmail.com>
2026-01-14 18:44:04 +0100

    log OrderServiceException

--- src/main/java/com/islamhamada/petshop/exception/RestResponseEntityExceptionHandler.java
+++ src/main/java/com/islamhamada/petshop/exception/RestResponseEntityExceptionHandler.java
@@ -2,16 +2,19 @@ package com.islamhamada.petshop.exception;
 
 import com.islamhamada.petshop.contracts.exception.FeignClientException;
 import com.islamhamada.petshop.contracts.model.RestExceptionResponse;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+@Log4j2
 @ControllerAdvice
 public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(OrderServiceException.class)
     public ResponseEntity<RestExceptionResponse> handleOrderServiceException(OrderServiceException exception){
+        log.error(exception);
         return new ResponseEntity<>(RestExceptionResponse.builder()
                 .error_code(exception.getError_code())
                 .error_message(exception.getMessage())
