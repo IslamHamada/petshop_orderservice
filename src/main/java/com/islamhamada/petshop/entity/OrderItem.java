3db7bd126869489bc18b8c54f771289284d90d66
IslamHamada <islamhamada222@gmail.com>
2026-01-22 13:22:22 +0100

    add JPA reltationship between Order and OrderItem

--- src/main/java/com/islamhamada/petshop/entity/OrderItem.java
+++ src/main/java/com/islamhamada/petshop/entity/OrderItem.java
@@ -3,6 +3,7 @@ package com.islamhamada.petshop.entity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -18,7 +19,8 @@ public class OrderItem {
     @GeneratedValue
     private long id;
 
-    private long orderId;
+    @ManyToOne
+    private Order order;
 
     private long productId;
 
