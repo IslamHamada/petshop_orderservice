51b70a09893f478a28c269e83f1b3f57de22fb8b
IslamHamada <islamhamada222@gmail.com>
2026-01-27 14:43:12 +0100

    move handling feign client exception and invalid arguments to ServiceExceptionHandler

--- src/main/java/com/islamhamada/petshop/exception/RestResponseEntityExceptionHandler.java
+++ src/main/java/com/islamhamada/petshop/exception/RestResponseEntityExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.islamhamada.petshop.exception;
 
 import com.islamhamada.petshop.contracts.exception.FeignClientException;
+import com.islamhamada.petshop.contracts.exception.ServiceExceptionHandler;
 import com.islamhamada.petshop.contracts.model.RestExceptionResponse;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.ResponseEntity;
@@ -11,7 +12,7 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 
 @Log4j2
 @ControllerAdvice
-public class RestResponseEntityExceptionHandler {
+public class RestResponseEntityExceptionHandler extends ServiceExceptionHandler {
 
     @ExceptionHandler(OrderServiceException.class)
     public ResponseEntity<RestExceptionResponse> handleOrderServiceException(OrderServiceException exception){
@@ -20,23 +21,5 @@ public class RestResponseEntityExceptionHandler {
                 .error_code(exception.getError_code())
                 .error_message(exception.getMessage())
                 .build(), exception.getHttpStatus());
-    }
-
-    @ExceptionHandler(FeignClientException.class)
-    public ResponseEntity<RestExceptionResponse> handleOrderServiceException(FeignClientException exception){
-        log.error(exception);
-        return new ResponseEntity<>(RestExceptionResponse.builder()
-                .error_code(exception.getError_code())
-                .error_message(exception.getMessage())
-                .build(), exception.getHttpStatus());
-    }
-
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<RestExceptionResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException exception){
-        log.error(exception);
-        return new ResponseEntity<>(RestExceptionResponse.builder()
-                .error_code(exception.getDetailMessageCode())
-                .error_message(exception.getMessage())
-                .build(), exception.getStatusCode());
     }
 }
