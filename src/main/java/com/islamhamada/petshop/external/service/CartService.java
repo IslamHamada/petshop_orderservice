0ef04826e725fb40c18602be741ad5e1592e04f4
IslamHamada <islamhamada222@gmail.com>
2026-01-30 19:56:41 +0100

    use wiremock properties if available before calling the loadbalancer

--- src/main/java/com/islamhamada/petshop/external/service/CartService.java
+++ src/main/java/com/islamhamada/petshop/external/service/CartService.java
@@ -11,6 +11,7 @@ import java.util.List;
 
 @FeignClient(
         name = "CartService",
+        url = "${CartService.url:}",
         path = "/cart",
         fallbackFactory = CartServiceFallbackFactory.class
 )
--- src/main/java/com/islamhamada/petshop/external/service/ProductService.java
+++ src/main/java/com/islamhamada/petshop/external/service/ProductService.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 
 @FeignClient(
         name = "ProductService",
+        url = "${ProductService.url:}",
         path = "/product",
         fallbackFactory = ProductServiceFallbackFactory.class
 )
